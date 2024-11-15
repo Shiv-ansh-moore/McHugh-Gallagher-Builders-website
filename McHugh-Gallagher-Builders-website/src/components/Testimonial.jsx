20f241895a30fed6073cdf987cee3c1791814d4c
shivansh moore <shivanshmoore05@gmail.com>
2024-11-15 06:48:54 +0000

    reviews fixed

--- McHugh-Gallagher-Builders-website/src/components/Testimonial.jsx
+++ McHugh-Gallagher-Builders-website/src/components/Testimonial.jsx
@@ -5,12 +5,12 @@ const Testimonial = ({ name, text }) => {
       <div className="flex justify-center">
         <h1 className="text-4xl p-2 font-bold">Reviews</h1>
       </div>
-
+      
       {/* Content */}
       <div className="flex justify-center">
-        <div className="text-center pt-2 max-w-[70vw] md:max-w-[50vw] overflow-hidden text-ellipsis">
-          <h2 className="italic text-3xl font-bold truncate">{name}</h2>
-          <p className="italic text-lg line-clamp-4 break-words overflow-hidden">
+        <div className="text-center pt-2 px-4 max-w-[90vw] md:max-w-[50vw] overflow-hidden text-ellipsis">
+          <h2 className="italic text-2xl md:text-3xl font-bold truncate">{name}</h2>
+          <p className="italic text-base md:text-lg line-clamp-4 break-words overflow-hidden">
             "{text}"
           </p>
         </div>
@@ -20,3 +20,4 @@ const Testimonial = ({ name, text }) => {
 };
 
 export default Testimonial;
+
